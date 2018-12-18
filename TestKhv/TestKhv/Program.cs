2560ad4e4516300f80d0f50f70c46ea5d9761bd0
Khvicha <Khvicha@FREESKY>
2018-12-19 03:27:32 +0400

    KhvChanges

---
+++ TestKhv/TestKhv/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestKhv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
